c8022ccf64d8d74cbd659afbb9b677c089938769
Nick Cruz <nickcruz920@gmail.com>
2023-02-23 16:19:37 -0500

    Created theArray function

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function closeContactUs() {
   
   contactModal.style.display = "";
 }
+
+<!-- Create an "Onload" function to Iterate through an array and display it's contents -->
+function theArray() {
+  let myIcon = `<i class="fa-solid fa-user text-light fs-1"></i>`;
+  let myArray = [`$<h2> The Type of Icon is:${myIcon}<button>Contact Me</button>`,
+                 `$<h2> The Type of Icon is:${myIcon}<button>Contact Me</button>`,
+                 `$<h2> The Type of Icon is:${myIcon}<button>Contact Me</button>`];
+
+let text = "";
+for (let i = 0; i < myArray.length; i++) {
+  text += myArray[i] + "<br>";
+}
+  
+  document.getElementById("output").innerHTML = text;
+}
