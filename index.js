c1604b56a2bd50f2f1c3e3386cb0aa6ead33ed47
Nick Cruz <nickcruz920@gmail.com>
2023-02-25 21:46:09 -0500

    Added countDownSpecial function

--- index.js
+++ index.js
@@ -55,6 +55,41 @@ function checkTime(i) {
   return i;
 }
 
+function countDownSpecial() {
+ 
+
+// Set the date we're counting down to
+var countDownDate = new Date("August, 2023 15:37:25").getTime();
+
+// Update the count down every 1 second
+
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("sale").innerHTML = " Time Until Sale Ends: " + " " + days + " days " + hours + " hours "
+  + minutes + " minutes " + seconds + " seconds ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("sale").innerHTML = "SALE HAS OFFICIALLY ENDED";
+  }
+}, 1000);
+  
+}
+
 <!-- Create a Function where when clicked on it displays 3 modals simultaneously (and make this responsive).. Likely need to use querySelectorAll OR do what I did above and create a template literal and then iterate through. 
 
 
